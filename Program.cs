e70e5e15d9d413b3fe61e5016aa3711aaf859744
Carlos Balcazar <cbalcazarp@gmail.com>
2021-09-07 23:33:27 -0500

    new code added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practica_2_preg_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
